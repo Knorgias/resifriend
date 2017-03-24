e6face722778ffd467d9b164ca341d64cd4dc4a5
Kostas Norgias <kostasnorgias@gmail.com>
2017-03-24 22:21:06 +0100

    added new view files

--- index.js
+++ index.js
@@ -9,13 +9,17 @@ const port = 9090
 
 /*Specify the template engine*/
 app.set('view engine', 'pug')
-
 app.set('views', './views') //This defaults to the views directory in the application root directory
+//Use a public folder
+app.use( express.static(__dirname + '/public'))
+
 
 //GET request for index
 app.get('/', function (req, res) {
-  res.render('index', { title: 'Hey', message: 'Hello there!' })
+  res.render('index', { title: 'Home Sweet Home', message: 'The sweetest deals on your new home!' })
 })
+
+
 
 /*Shoot the server*/
 app.listen( port, function () {
