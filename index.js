89d10630a9ed9949a00956c95009a8c381e18870
Kostas Norgias <kostasnorgias@gmail.com>
2017-03-24 13:04:04 +0100

    Setup pug view template

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
-const express = require ('express')
+/*Make necessary requires*/
+const express = require( 'express' )
+const pug = require( 'pug' )
 let app = express()
-let db = require(__dirname + '/modules/database')
-let appPort = 9090
+let db = require( __dirname + '/modules/database' )
 
-app.listen(appPort, function () {
-  console.log('Application listening on port ' + appPort + '!')
+/*Set the port*/
+const port = 9090
+
+/*Specify the template engine*/
+app.set('view engine', 'pug')
+
+app.set('views', './views') //This defaults to the views directory in the application root directory
+
+//GET request for index
+app.get('/', function (req, res) {
+  res.render('index', { title: 'Hey', message: 'Hello there!' })
+})
+
+/*Shoot the server*/
+app.listen( port, function () {
+  console.log( 'Application listening on port ' + port + '!' )
 })
